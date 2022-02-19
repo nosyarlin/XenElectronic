569b71f00aafd217cdcd5cb9a0b8dd071220de13
Rayson Lim Jun Kai <rayson@jublia.com>
2022-02-19 20:41:12 +0800

    Change how query is formed in test_product_resource to remove hardcoding

--- swagger_server/test/functional/test_resources.py
+++ swagger_server/test/functional/test_resources.py
@@ -2,7 +2,10 @@
 
 def test_product_resource(test_client, init_database):
     # response = test_client.get('/product')
-    response = test_client.get('/product?category=home appliances')
+    response = test_client.get(
+        '/search',
+        query_string={'category': 'home appliances'}
+    )
     product = response.json[0]
     assert product['name'] == 'refrigerator'
     assert product['price'] == 999.99
