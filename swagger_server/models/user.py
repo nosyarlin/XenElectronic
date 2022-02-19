0707955a0804ab9b2fbd53d2c678d54b9d66ae0d
Rayson Lim Jun Kai <rayson@jublia.com>
2022-02-19 14:36:55 +0800

    Add methods for common queries for User model

--- swagger_server/models/user.py
+++ swagger_server/models/user.py
@@ -12,3 +12,19 @@ class User(db.Model):
     def __init__(self, username, password):
         self.username = username
         self.password = password
+
+    @classmethod
+    def find_by_username(cls, username):
+        return cls.query.filter_by(username=username).first()
+
+    @classmethod
+    def find_by_id(cls, id):
+        return cls.query.filter_by(id=id).first()
+
+    def save_to_db(self):
+        db.session.add(self)
+        db.session.commit()
+
+    def delete_from_db(self):
+        db.session.delete(self)
+        db.session.commit()
