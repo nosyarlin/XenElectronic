b5355209ea6597a44be4f6e584012412804275a8
Rayson Lim Jun Kai <rayson@jublia.com>
2022-02-19 14:58:31 +0800

    Write init method for ProductCategory class properly

--- swagger_server/models/product_category.py
+++ swagger_server/models/product_category.py
@@ -11,9 +11,7 @@ class ProductCategory(Model):
     product = db.relationship("Product", back_populates="product_categories")
     categoryId = db.Column(db.Integer, db.ForeignKey('category.id'))
     category = db.relationship("Category", back_populates="product_categories")
-    quantity = db.Column(db.Integer)
 
-    def __init__(self, checkoutId, productId, quantity):
-        self.checkoutId = checkoutId
+    def __init__(self, productId, categoryId):
         self.productId = productId
-        self.quantity = quantity
+        self.categoryId = categoryId
