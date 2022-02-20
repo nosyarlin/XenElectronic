1afdc0353078a334830d870570eae7d1fb400509
Rayson Lim Jun Kai <rayson@jublia.com>
2022-02-20 17:29:03 +0800

    Hardcode calling the login api to get cookies set.

--- client/src/components/screens/Home.js
+++ client/src/components/screens/Home.js
@@ -4,19 +4,37 @@ import Card from '../parts/Card';
 import { DefaultApi } from 'xen_electronics_web_store';
 
 export default function Home() {
-    const [category, setCategory] = useState('home appliances')
-    const [products, setProducts] = useState([])
+    const [category, setCategory] = useState('home appliances');
+    const [products, setProducts] = useState([]);
 
+    // Query for products
     useEffect(() => {
         const apiInstance = new DefaultApi();
         apiInstance.search({category}, (error, data, response) => {
             if (error) {
                 console.error(error);
             } else {
-                setProducts(data)
+                setProducts(data);
             }
         })
-    }, [category])
+    }, [category]);
+
+    // Login
+    // Hardcoded for MVP instead of triggered by user behavior
+    useEffect(() => {
+        const apiInstance = new DefaultApi();
+        const requestBody = {
+            credentials: {
+                username: 'TestUser',
+                password: 'password'
+            }
+        }
+        apiInstance.login(
+            requestBody,
+            (error, data, response) => {
+                console.log(response);
+            });
+    });
 
     return(
         <div>
