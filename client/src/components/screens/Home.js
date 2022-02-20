5cf0ab0e5ba7576d5b90793ecd33c99bea9af1b5
Rayson Lim Jun Kai <rayson@jublia.com>
2022-02-20 16:09:24 +0800

    Query for fake data in front end to make sure that react app can talk to flask server

--- client/src/components/screens/Home.js
+++ client/src/components/screens/Home.js
@@ -1,17 +1,25 @@
 import React, { useState, useEffect } from 'react';
-import Navbar from '../parts/Navbar'
+import Navbar from '../parts/Navbar';
+import Card from '../parts/Card';
+import { DefaultApi } from 'xen_electronics_web_store';
 
 export default function Home() {
     const [category, setCategory] = useState('home appliances')
 
+    let apiInstance = new DefaultApi();
+
     useEffect(() => {
-        console.log(category)
+        apiInstance.search({category}, (error, data, response) => {
+            console.log(error)
+            console.log(data)
+        })
     })
 
     return(
         <div>
             <Navbar onClick={setCategory}/>
             <div className="container">
+                <Card/>
             </div>
         </div>
     )
