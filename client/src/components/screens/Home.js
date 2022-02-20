afbe297ae4ce918e6b8f206c52a20f66841a70d4
Rayson Lim Jun Kai <rayson@jublia.com>
2022-02-20 16:31:34 +0800

    Render product data from server on cards

--- client/src/components/screens/Home.js
+++ client/src/components/screens/Home.js
@@ -5,21 +5,31 @@ import { DefaultApi } from 'xen_electronics_web_store';
 
 export default function Home() {
     const [category, setCategory] = useState('home appliances')
-
-    let apiInstance = new DefaultApi();
+    const [products, setProducts] = useState([])
 
     useEffect(() => {
+        const apiInstance = new DefaultApi();
         apiInstance.search({category}, (error, data, response) => {
-            console.log(error)
-            console.log(data)
+            if (error) {
+                console.error(error);
+            } else {
+                setProducts(data)
+            }
         })
-    })
+    }, [category])
 
     return(
         <div>
             <Navbar onClick={setCategory}/>
             <div className="container">
-                <Card/>
+                {products.map(
+                    product => (
+                    <Card
+                        key={product.id}
+                        id={product.id}
+                        name={product.name}
+                        price={product.price}
+                    />))}
             </div>
         </div>
     )
